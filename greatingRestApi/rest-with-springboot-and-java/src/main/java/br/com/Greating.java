93b7119ea55856488431e2034a8f7f7e91048abe
felipelopes94 <felipee.c.lopess@gmail.com>
2024-09-17 20:47:38 -0300

    commit de primeiros arquivos

---
+++ greatingRestApi/rest-with-springboot-and-java/src/main/java/br/com/Greating.java
@@ -0,0 +1,23 @@
+package br.com;
+
+public class Greating {
+	
+	private final long id;
+	private final String content;
+	
+	public Greating(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+	
+	
+    
+}
---
+++ greatingRestApi/rest-with-springboot-and-java/src/main/java/br/com/GreatingController.java
@@ -0,0 +1,20 @@
+package br.com;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class GreatingController {
+
+	private static final String template = "Hello, %s!";
+	private static final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping("/greating")
+	public Greating greating(@RequestParam(value = "name", defaultValue = "World")String name) {
+		return new Greating(counter.incrementAndGet(), String.format(template, name));
+	}
+}
